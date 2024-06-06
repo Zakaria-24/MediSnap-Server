8a6d02b9871744b5490ac9d056a5a0d924fb285f
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-06-06 07:24:50 +0600

    get user data by email

--- index.js
+++ index.js
@@ -97,6 +97,12 @@ async function run() {
       res.send(result)
     })
 
+      // get a user info by email from db
+      app.get('/user/:email', async (req, res) => {
+        const email = req.params.email
+        const result = await usersCollection.findOne({ email })
+        res.send(result)
+      })
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
