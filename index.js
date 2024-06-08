53518b6a82c109138b21236d5b855dfdbef7841d
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-06-09 00:24:44 +0600

    successfully delate a category by delete request in categories collection

--- index.js
+++ index.js
@@ -180,6 +180,16 @@ async function run() {
         res.send(result)
       })
 
+
+      // delete a category from categories collection
+      app.delete('/category/:id', verifyToken, verifyAdmin, async (req, res) => {
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const result = await categoriesCollection.deleteOne(query)
+        // {_id: new ObjectId(req.params.id)}
+        res.send(result)
+      })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
