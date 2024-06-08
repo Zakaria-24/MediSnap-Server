2cb5b361df2b6baac58210a8405b59f49209a0d6
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-06-09 02:46:03 +0600

    update a categoey succcessfully

--- index.js
+++ index.js
@@ -190,6 +190,18 @@ async function run() {
         res.send(result)
       })
 
+      // update a category from categories collection
+      app.put('/category/:id', verifyToken, verifyAdmin, async (req, res) => {
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const updateCategory = req.body
+        const updateDoc = {
+          $set: updateCategory
+        }
+        const result = await categoriesCollection.updateOne(query, updateDoc)
+        res.send(result)
+      })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
