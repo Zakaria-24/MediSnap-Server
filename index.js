6ac22bac393107284348df131c108a7843f39bb7
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-06-12 23:01:49 +0600

    use local storate for token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const cors = require('cors')
-const cookieParser = require('cookie-parser')
+// const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const jwt = require('jsonwebtoken')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
@@ -10,31 +10,48 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 8000
 
 // middleware
-const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
-  credentials: true,
-  optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+// const corsOptions = {
+//   origin: ['http://localhost:5173', 'http://localhost:5174'],
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// }
+app.use(cors())
 
 app.use(express.json())
-app.use(cookieParser())
+// app.use(cookieParser())
 
 // Verify Token Middleware
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token
-  // console.log(token)
-  if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
+// const verifyToken = async (req, res, next) => {
+//   const token = req.cookies?.token
+//   // console.log(token)
+//   if (!token) {
+//     return res.status(401).send({ message: 'unauthorized access' })
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       console.log(err)
+//       return res.status(401).send({ message: 'unauthorized access' })
+//     }
+//     req.user = decoded
+//     next()
+//   })
+// }
+
+const verifyToken =async (req,res,next) => {
+  console.log('inside verify token',req.headers.authorization);
+  if(!req.headers.authorization){
+    return res.status(401).send({message:'unauthorized access'})
   }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
+  const token = req.headers.authorization.split(' ')[1]
+  console.log(process.env.ACCESS_TOKEN_SECRET)
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({message:'bed request invalid token'})
     }
-    req.user = decoded
-    next()
+    req.decoded = decoded;
+    next();
   })
+  
 }
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rfjtmur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -57,75 +74,122 @@ async function run() {
     const paymentsCollection = db.collection('payments')
 
       // verify admin middleware
-      const verifyAdmin = async (req, res, next) => {
-        console.log('hello')
-        const user = req.user
-        const query = { email: user?.email }
-        const result = await usersCollection.findOne(query)
-        console.log(result?.role)
-        if (!result || result?.role !== 'admin')
-          return res.status(401).send({ message: 'unauthorized access!!' })
+      // const verifyAdmin = async (req, res, next) => {
+      //   console.log('hello')
+      //   const user = req.user
+      //   const query = { email: user?.email }
+      //   const result = await usersCollection.findOne(query)
+      //   console.log(result?.role)
+      //   if (!result || result?.role !== 'admin')
+      //     return res.status(401).send({ message: 'unauthorized access!!' })
   
-        next()
-      }
+      //   next()
+      // }
+
+
+      //verify admin
+  const verifyAdmin = async(req,res,next) => {
+    const email = req.decoded.email;
+    console.log('inside',email);
+    const query = {email: email};
+    const user = await usersCollection.findOne(query);
+    const isAdmin = user?.role === 'admin'
+    if(!isAdmin){
+      return res.status(403).send({message:'forbidden access'})
+    }
+    next();
+  }
 
       // verify host middleware
-      const verifySeller = async (req, res, next) => {
-        // console.log('hello')
-        const user = req.user
-        const query = { email: user?.email }
-        const result = await usersCollection.findOne(query)
-        console.log(result?.role)
-        if (!result || result?.role !== 'seller') {
-          return res.status(401).send({ message: 'unauthorized access!!' })
-        }
+      // const verifySeller = async (req, res, next) => {
+      //   // console.log('hello')
+      //   const user = req.user
+      //   const query = { email: user?.email }
+      //   const result = await usersCollection.findOne(query)
+      //   console.log(result?.role)
+      //   if (!result || result?.role !== 'seller') {
+      //     return res.status(401).send({ message: 'unauthorized access!!' })
+      //   }
   
-        next()
+      //   next()
+      // }
+
+      const verifySeller = async(req,res,next) => {
+        const email = req.decoded.email;
+        console.log('inside',email);
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        const isAdmin = user?.role === 'seller'
+        if(!isAdmin){
+          return res.status(403).send({message:'forbidden access'})
+        }
+        next();
       }
   
       // verify host middleware
-      const verifyUser = async (req, res, next) => {
-        // console.log('hello')
-        const user = req.user
-        const query = { email: user?.email }
-        const result = await usersCollection.findOne(query)
-        console.log(result?.role)
-        if (!result || result?.role !== 'user') {
-          return res.status(401).send({ message: 'unauthorized access!!' })
-        }
+      // const verifyUser = async (req, res, next) => {
+      //   // console.log('hello')
+      //   const user = req.user
+      //   const query = { email: user?.email }
+      //   const result = await usersCollection.findOne(query)
+      //   console.log(result?.role)
+      //   if (!result || result?.role !== 'user') {
+      //     return res.status(401).send({ message: 'unauthorized access!!' })
+      //   }
   
-        next()
+      //   next()
+      // }
+
+      const verifyUser = async(req,res,next) => {
+        const email = req.decoded.email;
+        console.log('inside',email);
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        const isAdmin = user?.role === 'user'
+        if(!isAdmin){
+          return res.status(403).send({message:'forbidden access'})
+        }
+        next();
       }
 
     // auth related api
-    app.post('/jwt', async (req, res) => {
-      const user = req.body
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '365d',
+    // app.post('/jwt', async (req, res) => {
+    //   const user = req.body
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: '365d',
+    //   })
+    //   res
+    //     .cookie('token', token, {
+    //       httpOnly: true,
+    //       secure: process.env.NODE_ENV === 'production',
+    //       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    //     })
+    //     .send({ success: true })
+    // })
+
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'1h',
       })
-      res
-        .cookie('token', token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-        })
-        .send({ success: true })
+      res.send({token})
     })
+
     // Logout
-    app.get('/logout', async (req, res) => {
-      try {
-        res
-          .clearCookie('token', {
-            maxAge: 0,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-          })
-          .send({ success: true })
-        // console.log('Logout successful')
-      } catch (err) {
-        res.status(500).send(err)
-      }
-    })
+    // app.get('/logout', async (req, res) => {
+    //   try {
+    //     res
+    //       .clearCookie('token', {
+    //         maxAge: 0,
+    //         secure: process.env.NODE_ENV === 'production',
+    //         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    //       })
+    //       .send({ success: true })
+    //     // console.log('Logout successful')
+    //   } catch (err) {
+    //     res.status(500).send(err)
+    //   }
+    // })
 
 
 
